2a2519625d0b66e680019a3a0167216d4c7108fd
dubeyTech <dubeyprashant583@gmail.com>
2021-01-16 14:15:21 +0530

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs");
 
 /////////////////// Home
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {yourZodiac: "Welcome!"});
 });
 
 ////////////////////// Choosing Your Zodiac Start
